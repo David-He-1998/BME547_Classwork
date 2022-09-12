1844477063de7c1c4f88b282193982aa7a7da672
David-He-1998 <heziwaidavid@gmail.com>
2022-09-12 12:26:36 -0400

    fix output format

---
+++ output.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 12:22:56 2022
+
+@author: david
+"""
+weight = 20 / 2.205
+dosage = weight * 30
+
+print("CORRECT DOSAGE")
+print("For a patient weighing {:.1f} kg,".format(weight))
+print("  the correct dosage is {} mg the first day".format(dosage))
