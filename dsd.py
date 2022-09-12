e736daec5009f01c607c4f62129521d57d433938
David-He-1998 <heziwaidavid@gmail.com>
2022-09-12 12:10:25 -0400

    File for classwork 9/12

---
+++ dsd.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 12:03:28 2022
+
+@author: david
+"""
+
+weight = 20 / 2.205
+dosage = weight * 30
+
+print("CORRECT DOSAGE")
+print("For a patient weighing {} kg,".format(weight))
+print("  the correct dosage is {} mg the first day".format(dosage))
+
+
