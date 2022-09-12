b587cc24212a4eb5f2aa2cbe786c815c59ce90ee
David-He-1998 <heziwaidavid@gmail.com>
2022-09-12 12:13:03 -0400

    Classwork 9.12

---
+++ output.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 12:08:40 2022
+
+@author: david
+"""
+
+weight = 20 / 2.205
+dosage = weight * 30
+
+print("CORRECT DOSAGE")
+print("For a patient weighing {} kg,".format(weight))
+print("  the correct dosage is {} mg the first day".format(dosage))
