dced537df234b471ce5a6e2442ffcca2391bc9d6
David-He-1998 <heziwaidavid@gmail.com>
2022-09-21 12:45:32 -0400

    classwork for practicing unit test and git action

---
+++ test_weight_entry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pytest
+
+
+@pytest.mark.parametrize("input, expected", [
+    ("22 lb", 10),
+    ("50 kg", 50),
+    ("35.6 kg", 36),
+    ("-20 kg", -10),
+    ("22 pound", 10),
+    ("22 lb, 50 kg", None),
+    ("22", None),
+    ("ten kg", 10)])
+def test_parse_weight_input(input, expected):
+    from weight_entry import parse_weight_input
+    answer = parse_weight_input(input)
+    assert answer == expected
