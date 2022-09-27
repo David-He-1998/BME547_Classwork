8b08c23984de3f59527570387a633d70cef3ee18
David-He-1998 <heziwaidavid@gmail.com>
2022-09-26 21:32:21 -0400

    Add the contents I missed during the classes

--- database2.py
+++ database2.py
@@ -45,14 +45,18 @@ def main():
     db.append(patient_entry("Bob Boyles", 2, 34))
     db.append(patient_entry("Charis Chou", 3, 23))
     return db
-    '''
-    '''
+
     db = {}
-    db[11] = (patient_entry("Ann", "Ables", 1, 30, ''))
-    db[22] = (patient_entry("Bob", "Boyles", 2, 34, ''))
-    db[33] = (patient_entry("Charis", "Chou", 3, 23, ''))
+    db[11] = (patient_entry("Ann", "Ables", 11, 30, ''))
+    db[22] = (patient_entry("Bob", "Boyles", 22, 34, ''))
+    db[3] = (patient_entry("Charis", "Chou", 3, 23, ''))
     return db
     '''
+    db = {}
+    db[11] = Patient("Ann", "Ables", 11, 30, [])
+    db[22] = Patient("Bob", "Boyles", 2, 34, [])
+    db[3] = Patient("Charis", "Chou", 3, 23, [])
+    return db
 
 
 def list_all(db):
@@ -68,13 +72,13 @@ def search(db, index):
     return False, False
     '''
     for patient in db:
-        if db[patient]['Id'] == index:
+        if db[patient].Id == index:
             return patient
     return False
 
 
-def add(entry, test, test_result):
-    entry[test] = (test_result)
+def add(entry, Test, test_result):
+    entry.test = (Test, test_result)
     return entry
 
 
@@ -92,17 +96,21 @@ def patient_entry(First_name, Last_name, ID, Age, Tests):
 
 
 def print_db(db):
-    for patient in db:
+    for patient_key in db:
         # print("Name: {}, Id: {}, age: {}".format(get_full_name(db[patient]),
         #                                         db[patient]["Id"],
         #                                         db[patient]["Age"]))
-        print("Name: {}, Id: {}, age: {}".format(db[patient].get_full_name(),
-                                                 db[patient]["Id"],
-                                                 db[patient]["Age"]))
+        print("Name: {}, Id: {}, age: {}".format(
+                db[patient_key].get_full_name(), db[patient_key].Id,
+                db[patient_key].age, db[patient_key].test))
+    print("Method 2")
+    for patient in db.values():
+        print("Name: {}, Id: {}, age: {}".format(patient.get_full_name(),
+              patient.Id, patient.age, patient.test))
 
 
 def adult_or_minor(patient):
-    if patient["Age"] >= 18:
+    if patient.age >= 18:
         return "Adult"
     else:
         return "Minor"
@@ -127,9 +135,15 @@ if __name__ == "__main__":
         add(db[patient], 'HDL', 40)
         print_db(db)
     '''
-    patient = Patient("Ann", "Ables", 1, 30, ['HDL', 40])
+    patient = Patient("Ziwei", "He", 44, 30, ['HDL', 40])
     print("Full name:{}, ID:{}, Age:{}, test:{}".format(
           patient.get_full_name(), patient.Id, patient.age, patient.test))
     print(type(patient))
     # patient1 = patient_entry("Ann", "Ables", 1, 30, ["HDL", 40])
     # print("{}, age {}".format(patient1.get_full_name(), patient1.age))
+    db = main()
+    print_db(db)
+    index = search(db, 3)
+    add(db[index], "HDL", 100)
+    print("Patient {} is {}".format(db[index].get_full_name(),
+          adult_or_minor(db[index])))
